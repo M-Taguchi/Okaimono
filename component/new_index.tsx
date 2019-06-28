fb11f1d3d0ca752d9cb20ea9d9d416b2eb2114f5
M-Taguchi <ponpocori.chiccyorina@gmail.com>
2019-06-28 18:03:22 +0900

    mobxとmobx-react-liteを追加

---
+++ component/new_index.tsx
@@ -0,0 +1,29 @@
+import {action, computed, configure, observable} from "mobx";
+configure({enforceActions: true});
+
+class new_index{
+    @observable currentText:string;
+    @observable items: string[];
+
+    @computed get isAddable():boolean{
+        return this.currentText != "";
+}
+
+    constructor(currentText: string = "", items: string[] = []) {
+        this.currentText = currentText;
+        this.items = items;
+    }
+
+    @action.bound
+    change_current_text(text:string){
+        this.currentText = text;
+    }
+
+    @action.bound
+    add_item(){
+        this.items.push(this.currentText);
+        this.currentText = "";
+    }
+}
+
+export default new_index;
